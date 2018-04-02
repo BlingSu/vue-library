0f84a4a6e194ea8af2a3c6a9274b9ca93cbee69f
angelasubi <21978301@qq.com>
2018-04-02 16:43:20 +0800

    feat: vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import App from './App'
 import router from './router/index'
+import Vuex from 'vuex'
 
 import axiosPlugin from 'common/js/server'
 Vue.use(axiosPlugin)
 
 Vue.use(ElementUI)
+Vue.use(Vuex)
 
 new Vue({
   el: '#app',
